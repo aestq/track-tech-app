af31eae4bdd73ae63bdc5b4a01a9d81ba2e130ba
aestq <aestq.dev@gmail.com>
2025-05-20 22:24:51 +0300

    api

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import path from 'path'
 import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import svgr from 'vite-plugin-svgr'
 
-export default defineConfig(({ command }) => {
+export default defineConfig(({ command, mode }) => {
+    const env = loadEnv(mode, process.cwd(), '')
+
     return {
         plugins: [
             react(),
@@ -23,7 +25,7 @@ export default defineConfig(({ command }) => {
             },
         },
         define: {
-            __API__: JSON.stringify(''),
+            __API__: JSON.stringify(env.VITE_PROD_API_URL),
             __IS_DEV__: JSON.stringify(command === 'serve'),
             __PROJECT__: JSON.stringify('frontend'),
         },
